3713a771f1ed99bf14b959b800873ad1e744c8c3
Eduardo Almeida <eduardoeandroid@gmail.com>
2020-09-17 19:02:14 -0300

    login

--- gestao_rh/urls.py
+++ gestao_rh/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', include('apps.core.urls')),
     path('admin/', admin.site.urls),
     path('funcionarios/', include('apps.funcionarios.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
