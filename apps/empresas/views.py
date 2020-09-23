664361634f06660074e6fa8fa71e9023206ce801
Eduardo Almeida <eduardoeandroid@gmail.com>
2020-09-22 21:15:09 -0300

    Create Empresa

--- apps/empresas/views.py
+++ apps/empresas/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from .models import Empresa
@@ -9,4 +10,7 @@ class EmpresaCreate(CreateView):
     def form_valid(self, form):
         obj = form.save()
         funcionario = self.request.user.funcionario
+        funcionario.empresa = obj
+        funcionario.save()
+        return HttpResponse('Ok')
 
