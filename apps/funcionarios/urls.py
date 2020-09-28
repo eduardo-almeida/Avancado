d02e8eb7ae980055df7443d9ec45218c556c0fad
Eduardo Almeida <eduardoeandroid@gmail.com>
2020-09-28 15:22:36 -0300

    Atualizacao de arquivos

--- apps/funcionarios/urls.py
+++ apps/funcionarios/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import FuncionariosList
 
 urlpatterns = [
-    path('', FuncionariosList.as_view, name='list_funcionarios'),
+    path('', FuncionariosList.as_view(), name='list_funcionarios'),
 ]
