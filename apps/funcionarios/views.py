2a0bbae0755274c1c1aa41b1f448ba7ace7b61bc
Eduardo Almeida <eduardoeandroid@gmail.com>
2020-09-29 15:11:30 -0300

    CRUD Funcionario, update

--- apps/funcionarios/views.py
+++ apps/funcionarios/views.py
@@ -1,6 +1,15 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, UpdateView
 from .models import Funcionario
 
 
 class FuncionariosList(ListView):
     model = Funcionario
+    
+
+    def get_queryset(self):
+        empresa_logada = self.request.user.funcionario.empresa
+        return Funcionario.objects.filter(empresa=empresa_logada)
+
+class FuncionarioEdit(UpdateView):
+    model = Funcionario
+    fields = ['nome', 'departamentos']
