88b7ff096420a7b25c534dbe8d625a6fc68ac22f
Eduardo Almeida <eduardoeandroid@gmail.com>
2020-09-29 16:34:18 -0300

    CRUD Funcionario, create

--- apps/funcionarios/views.py
+++ apps/funcionarios/views.py
@@ -1,5 +1,6 @@
+from django.contrib.auth.models import User
 from django.urls import reverse_lazy
-from django.views.generic import ListView, UpdateView, DeleteView
+from django.views.generic import ListView, UpdateView, DeleteView, CreateView
 from .models import Funcionario
 
 
@@ -18,3 +19,14 @@ class FuncionarioEdit(UpdateView):
 class FuncionarioDelete(DeleteView):
     model = Funcionario
     success_url = reverse_lazy('list_funcionarios')
+
+class FuncionarioCreate(CreateView):
+    model = Funcionario
+    fields = ['nome', 'departamentos']
+
+    def form_valid(self, form):
+        funcionario = form.save(commit=False)
+        funcionario.empresa = self.request.user.funcionario.empresa
+        funcionario.user = User.objects.create(username=funcionario.nome.split(' ')[0] + funcionario.nome.split(' ')[1])
+        funcionario.save()
+        return super(FuncionarioCreate, self).form_valid(form)
