b9c38c7c18d1f482520c9183cceaefab5b50a836
Eduardo Almeida <eduardoeandroid@gmail.com>
2020-10-05 12:07:29 -0300

    crud documentos

--- gestao_rh/urls.py
+++ gestao_rh/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('apps.core.urls')),
@@ -8,5 +9,6 @@ urlpatterns = [
     path('funcionarios/', include('apps.funcionarios.urls')),
     path('departamentos/', include('apps.departamentos.urls')),
     path('empresas/', include('apps.empresas.urls')),
+    path('documentos/', include('apps.documentos.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-]
+] +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
